c106e383f27e6a6bc9e96ae0d9eefcbd6345b9f0
NavasMuhammed <navas3444a3@gmail.com>
2023-06-21 00:50:12 +0530

    selected update logic

--- web/frontend/components/cardSelector/cardSelector.jsx
+++ web/frontend/components/cardSelector/cardSelector.jsx
@@ -4,10 +4,22 @@ import { useAppBridge } from "@shopify/app-bridge-react";
 import axios from "axios";
 import { getSessionToken } from "@shopify/app-bridge/utilities";
 import "./cardSelector.css";
-import { imageUrlObject } from "./data";
 
 const CardSelector = () => {
-  const [selectedImages, setSelectedImages] = useState({});
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  useEffect(() => {
+    const fetchBadges = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/api/badges");
+        const badges = response.data;
+        setSelectedImages(badges);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchBadges();
+  }, []);
 
   const handleClick = async () => {
     try {
@@ -16,6 +28,8 @@ const CardSelector = () => {
         selectedImages
       );
       console.log(response);
+      //alert the user that the badges were saved
+      alert("Badges are saved");
     } catch (err) {
       console.log(err);
     }
@@ -25,30 +39,28 @@ const CardSelector = () => {
 
   return (
     <div className="badge--container">
-      <div>
-        {imageUrlObject.map((item, index) => (
-          <Image
-            key={index}
-            className={`badge--image ${item.state ? "selected" : ""}`}
-            source={item.url}
-            alt="badge"
-            onClick={() => {
-              item.state = !item.state;
-              setSelectedImages({ ...selectedImages, [item.name]: item });
-            }}
-          />
-        ))}
-      </div>
+      {selectedImages && selectedImages.length > 0 && (
+        <div>
+          {selectedImages.map((item, index) => (
+            <Image
+              key={index}
+              className={`badge--image ${item.state ? "selected" : ""}`}
+              source={item.url}
+              alt="badge"
+              onClick={() => {
+                setSelectedImages((prevState) => {
+                  const updatedImages = [...prevState];
+                  updatedImages[index].state = !updatedImages[index].state;
+                  return updatedImages;
+                });
+              }}
+            />
+          ))}
+        </div>
+      )}
       <Button primary onClick={handleClick}>
         SAVE
       </Button>
-      <div>
-        {Object.values(selectedImages).map((item, index) => (
-          <div key={index}>
-            <span>{JSON.stringify(item)}</span>
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
